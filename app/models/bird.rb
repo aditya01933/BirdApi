62825f5c72e5beaaeebaa93e52452a31c6a82713
aditya01933 <adityatiwari01933@gmail.com>
2016-09-10 23:46:09 +0530

    activemodel serializer added

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,8 +1,13 @@
 class Bird
   include Mongoid::Document
+  # fields
   field :name, type: String
   field :family, type: String
   field :continents, type: String
   field :added, type: DateTime, default: ->{ Date.today }
   field :visible, type: Mongoid::Boolean, default: false
+
+  # validations
+  validates :name, :family, :continents, presence: :true
+
 end
